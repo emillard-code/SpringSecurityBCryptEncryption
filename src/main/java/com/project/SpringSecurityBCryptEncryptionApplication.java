a4f8612026d6bca1d5a4979ae04375a78d03c226
Emerson Millard <emillard.code@gmail.com>
2022-07-12 18:40:29 -0700

    Minor formatting changes.

--- src/main/java/com/project/SpringSecurityBCryptEncryptionApplication.java
+++ src/main/java/com/project/SpringSecurityBCryptEncryptionApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringSecurityBCryptEncryptionApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SpringSecurityBCryptEncryptionApplication.class, args);
+
     }
 
 }
